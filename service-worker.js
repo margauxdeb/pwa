76074d93b0d92c6c962de5616e54c12987e4f77d
margauxdeb <margauxdeb@gmail.com>
2018-03-12 06:34:47 -0400

    Modification fecth

--- service-worker.js
+++ service-worker.js
@@ -12,8 +12,24 @@ self.addEventListener('install', function(e) {
 self.addEventListener('fetch', function(event) {
   console.log(event.request.url);
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+    fetch(event.request).then(function(response) {
+      caches.open("tinyPWA").then(function(cache) {
+        cache.match(event.request).then(function (){
+              cache.put(event.request, response.clone());
+              console.log("cache updated");
+        });
+      });
+      console.log("Response from network");
+      return response.clone();
+    }).catch(function (){
+      return caches.match(event.request).then(function(response) {
+        if(response){
+          console.log("Response from cache");
+          return response;
+        } else {
+          fetch(event.request.clone());
+        }
+      });
     })
-  );
+  )
 });
