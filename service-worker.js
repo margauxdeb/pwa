cc507c1cd819344b2ac7aa889fe1b830db7591ab
margauxdeb <margauxdeb@gmail.com>
2018-03-12 06:37:21 -0400

    Modification fecth

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('tinyPWA').then(function(cache) {
-     return cache.addAll([
-       '/',
-       '/index.html'
-     ]);
-   })
- );
+  e.waitUntil(
+    caches.open('tinyPWA').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
@@ -14,16 +14,16 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).then(function(response) {
       caches.open("tinyPWA").then(function(cache) {
-        cache.match(event.request).then(function (){
-              cache.put(event.request, response.clone());
-              console.log("cache updated");
+        cache.match(event.request).then(function() {
+          cache.put(event.request, response.clone());
+          console.log("cache updated");
         });
       });
       console.log("Response from network");
       return response.clone();
-    }).catch(function (){
+    }).catch(function() {
       return caches.match(event.request).then(function(response) {
-        if(response){
+        if (response) {
           console.log("Response from cache");
           return response;
         } else {
@@ -32,4 +32,18 @@ self.addEventListener('fetch', function(event) {
       });
     })
   )
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return "tinyPWAV1" !== cacheName
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
 });
